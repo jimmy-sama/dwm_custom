b3f72217bbd139850022c402659f3ee2095a5137
jimmy-sama <lungmuss@proton.me>
2026-05-07 08:20:24 +0200

    remove dmenu references

--- config.h
+++ config.h
@@ -6,7 +6,6 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "BlexMono Nerd Font:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -71,8 +70,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofi[]  = { "rofi", "-show", "drun", "-theme", "~/.config/rofi/config.rasi", NULL };
 static const char *termcmd[]  = { "ghostty", NULL };
 
@@ -97,7 +94,6 @@ static const Key keys[] = {
 	{0, XF86XK_AudioPlay,			   spawn,	   SHCMD("rmpc remote keybind p")},
 	{0, XF86XK_AudioPrev,			   spawn,	   SHCMD("rmpc remote keybind '<'")},
 	{0, XF86XK_AudioNext,			   spawn,	   SHCMD("rmpc remote keybind '>'")},
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = rofi } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,	   spawn,          SHCMD("slock")},
